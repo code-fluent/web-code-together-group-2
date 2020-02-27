4a263eade857a29ab46a58b9acc2eb4e63b2539c
Tamás Pap <tamas@tamaspap.com>
2020-02-27 21:39:17 +0200

    Implement filtering todos.

--- projects/todo-app/web/todos/src/App.js
+++ projects/todo-app/web/todos/src/App.js
@@ -5,16 +5,41 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: []
+      todos: [],
+      text: "",
+      filter: "ALL"
     };
   }
 
-  componentDidMount() {
+  loadTodos = () => {
     axios.get("http://localhost:8080/todos").then(response => {
       this.setState({
         todos: response.data
       });
     });
+  };
+
+  createTodo = name => {
+    return axios.post("http://localhost:8080/todos/", {
+      name: name,
+      isCompleted: 0
+    });
+  };
+
+  updateTodo = (id, isCompleted) => {
+    return axios.put(`http://localhost:8080/todos/${id}`, {
+      isCompleted: isCompleted
+    });
+  };
+
+  setFilter = filter => {
+    this.setState({
+      filter: filter
+    });
+  };
+
+  componentDidMount() {
+    this.loadTodos();
   }
 
   onKeyPress = async event => {
@@ -22,42 +47,89 @@ class App extends Component {
       const name = event.currentTarget.value;
       event.currentTarget.value = "";
 
-      await axios.post("http://localhost:8080/todos", {
-        name: name,
-        isCompleted: 0
-      });
+      await this.createTodo(name);
+      this.loadTodos();
+    }
+  };
 
-      axios.get("http://localhost:8080/todos").then(response => {
-        this.setState({
-          todos: response.data
-        });
+  onInputChange = event => {
+    const text = event.currentTarget.value;
+    this.setState({
+      text: text
+    });
+  };
+
+  onCheckboxChange = async (id, event) => {
+    const isCompleted = event.currentTarget.checked;
+    await this.updateTodo(id, isCompleted);
+    this.loadTodos();
+  };
+
+  filterTodos = () => {
+    if (this.state.filter === "ALL") {
+      return this.state.todos;
+    }
+
+    if (this.state.filter === "COMPLETED") {
+      return this.state.todos.filter(todo => {
+        return todo.isCompleted === 1;
+      });
+    }
+
+    if (this.state.filter === "INCOMPLETED") {
+      return this.state.todos.filter(todo => {
+        return todo.isCompleted === 0;
       });
     }
   };
 
   render() {
+    const filteredTodos = this.filterTodos();
+
     return (
       <div className="container">
         <input
           type="text"
           className="addTodo"
           placeholder="Add todo"
+          value={this.state.text}
+          onChange={this.onInputChange}
           onKeyPress={this.onKeyPress}
         />
 
-        {this.state.todos.map(function(todo) {
+        {filteredTodos.map(todo => {
           return (
             <label className="todo" key={todo.id}>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                checked={todo.isCompleted}
+                onChange={event => this.onCheckboxChange(todo.id, event)}
+              />
               {todo.name}
             </label>
           );
         })}
 
+        <h1>{this.state.filter}</h1>
+
         <div className="filters">
-          <button className="filter">All</button>
-          <button className="filter">Completed</button>
-          <button className="filter">Incompleted</button>
+          <button className="filter" onClick={() => this.setFilter("ALL")}>
+            All
+          </button>
+
+          <button
+            className="filter"
+            onClick={() => this.setFilter("COMPLETED")}
+          >
+            Completed
+          </button>
+
+          <button
+            className="filter"
+            onClick={() => this.setFilter("INCOMPLETED")}
+          >
+            Incompleted
+          </button>
         </div>
       </div>
     );
